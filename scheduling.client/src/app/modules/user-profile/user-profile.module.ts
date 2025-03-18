66d1b3cdd129c30d3cefca1aec18bc5b410805cb
troy-miller0824 <troy54588@gmail.com>
2025-03-18 18:22:07 +0300

    Fix

--- scheduling.client/src/app/modules/user-profile/user-profile.module.ts
+++ scheduling.client/src/app/modules/user-profile/user-profile.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileListComponent } from './components/user-profile-list/user-profile-list.component';
 import { SharedModule } from '../../shared/shared.module';
+import { UserProfileManageComponent } from './components/user-profile-manage/user-profile-manage.component';
 
 
 @NgModule({
   declarations: [
-    UserProfileListComponent
+    UserProfileListComponent,
+    UserProfileManageComponent
   ],
   imports: [
     SharedModule,
